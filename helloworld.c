e80094039795e86777fa6334e2db45cacb2bf362
diegomarques1 <64621051+diegomarques1@users.noreply.github.com>
2022-02-14 18:45:14 -0300

    Arquivo inicial em C - Hello, World

---
+++ helloworld.c
@@ -0,0 +1,11 @@
+// Diego Souza Lima Marques - TIA: 32039921
+// Lucas de Camargo Gonçalves - TIA: 32074964
+// Computação Paralela - Lab01a
+
+#include <stdio.h>
+
+int main() 
+{
+  printf("Hello, World\n");
+  return 0;
+}
